08e488e6b27be4b77455c45e702d80499f6bb14f
kangsanycco <kangsanycco@gmail.com>
2026-02-06 03:59:30 +0900

    26.02.05_Team_Project_v30

---
+++ 5.Team_Project_Convey_v4_ver.446 (2)/5.Team_Project_Convey_v4_ver.446/Core/_Common/function.h
@@ -0,0 +1,46 @@
+/*
+ * includes.h
+ *
+ *  Created on: Jan 19, 2026
+ *      Author: kangs
+ */
+
+#ifndef COMMON_FUNCTION_H_
+#define COMMON_FUNCTION_H_
+
+#include "main.h"
+
+
+// drv_i2c.c
+HAL_StatusTypeDef DRV_I2C_WriteReg(uint8_t reg, uint8_t data);
+void DRV_I2C_Init(void);
+HAL_StatusTypeDef DRV_I2C_Robot_SendStart(void);
+
+// drv_uart.c
+void DRV_UART_Init(void);
+void DRV_UART_RxUpdate(uint8_t* p);
+void DRV_UART_TxReport(void);
+
+// bsp_sensor.c
+uint8_t BSP_Sensor_GetEmptyRackFloor(void);
+void BSP_Sensor_UpdateAll(void);
+
+// bsp_servo.c
+void BSP_Servo_SetSpeed(uint8_t ch, uint8_t speed);
+void BSP_Servo_Control_Speed(void);
+
+// bsp_stepper.c
+void BSP_Stepper_SetEnable(uint8_t enable);
+void BSP_Stepper_Home(void);
+void BSP_Stepper_MoveToFloor(uint8_t floor);
+void BSP_Stepper_Interrupt_Handler(void);
+
+
+// task_system.c
+void TASK_System_Execute(void);
+
+
+
+#endif /* COMMON_FUNCTION_H_ */
+
+
